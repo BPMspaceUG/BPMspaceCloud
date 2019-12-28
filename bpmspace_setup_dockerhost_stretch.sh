69c0f25c076cdbfd3aca2297ed70fc2fc0c8842e
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2019-12-28 14:25:58 +0100

    Update bpmspace_setup_dockerhost_stretch.sh

--- bpmspace_setup_dockerhost_stretch.sh
+++ bpmspace_setup_dockerhost_stretch.sh
@@ -81,7 +81,6 @@ apt update > /dev/null 2>&1
 
 echo " "
 echo "SET SSH Serever to 7070"
-mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 cd /home/rootmessages
 cp linux_config_script_files_II/dockerhost/daemon/sshd/sshd_config /etc/ssh/
 mkdir /home/rootmessages/.ssh
