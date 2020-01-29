3fcc1bab6dcab8538e5cd979b849f540c51edf59
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2020-01-29 11:51:33 +0100

    Update bpmspace_step_04_03_gluster.sh

--- bpmspace_step_04_03_gluster.sh
+++ bpmspace_step_04_03_gluster.sh
@@ -2,15 +2,15 @@
 mkdir /mnt/gluster/ -p
 mkdir /mnt/gluster/gluster_bpmspacecloud -p
 echo 'localhost:/gluster_bpmspacecloud /mnt/gluster/gluster_bpmspacecloud glusterfs defaults,_netdev,backupvolfile-server=localhost 0 0' >> /etc/fstab
-
-if [ "$DOCKER_SWARM_TYPE" == "MASTER" ]; then
-	cd /mnt/gluster/gluster_bpmspacecloud
-	git clone https://github.com/BPMspaceUG/BPMspaceCloud.git
-fi
+mount -a
 if [ "$DOCKER_SWARM_TYPE" == "NODE" ]; then
 	mkdir /mnt/gluster/gluster_bpmspacecloud_nodes -p
 	echo 'localhost:/gluster_bpmspacecloud_nodes /mnt/gluster/gluster_bpmspacecloud_nodes glusterfs defaults,_netdev,backupvolfile-server=localhost 0 0' >> /etc/fstab  
 fi
-rm -rf /root/BPMspaceCloud
 mount -a
+if [ "$DOCKER_SWARM_TYPE" == "MASTER" ]; then
+	cd /mnt/gluster/gluster_bpmspacecloud
+	git clone https://github.com/BPMspaceUG/BPMspaceCloud.git
+fi
+rm -rf /root/BPMspaceCloud
 df -h  
