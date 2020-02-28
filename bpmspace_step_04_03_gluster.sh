330ff47758b71708b16c88e86c1b3cf11546b256
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2020-02-28 18:22:19 +0100

    Update bpmspace_step_04_03_gluster.sh

--- bpmspace_step_04_03_gluster.sh
+++ bpmspace_step_04_03_gluster.sh
@@ -13,4 +13,8 @@ if [ "$DOCKER_SWARM_TYPE" == "MASTER" ]; then
 	git clone https://github.com/BPMspaceUG/BPMspaceCloud.git
 fi
 rm -rf /root/BPMspaceCloud
+docker plugin install --alias glusterfs trajano/glusterfs-volume-plugin --grant-all-permissions --disable
+docker plugin set glusterfs SERVERS=$DOCKERHOSTCLUSTERIP
+docker plugin enable glusterfs
+
 df -h  
